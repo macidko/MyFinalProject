4f2d62f2e04abcbd7a8ec5fcaa7fc08f18b8acef
Eren Macit <ernmctt@gmail.com>
2023-11-19 16:53:39 +0300

    10. Gün sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using Core.Utilities.Results;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
@@ -13,40 +14,59 @@ namespace ConsoleUI
             //DTO- DATA TRANSFORMATION OBJECTS
             //ProductTest();
             //CategoryTest();
+            //ProductDetailTest();
 
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if (result.Success == true)
             {
-                Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                    Console.WriteLine(DateTime.Now.Hour == 16);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
 
         }
 
-        private static void CategoryTest()
-        {
-            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-            foreach (var category in categoryManager.GetAll())
-            {
-                Console.WriteLine(category.CategoryName);
-            }
-        }
+        //private static void ProductDetailTest()
+        //{
+        //    ProductManager productManager = new ProductManager(new EfProductDal());
+        //    foreach (var product in productManager.GetProductDetails())
+        //    {
+        //        Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+        //    }
+        //}
 
-        private static void ProductTest()
-        {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+        //private static void CategoryTest()
+        //{
+        //    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        //    foreach (var category in categoryManager.GetAll())
+        //    {
+        //        Console.WriteLine(category.CategoryName);
+        //    }
+        //}
 
-            //foreach (var product in productManager.GetAll())
-            //{
-            //    Console.WriteLine(product.ProductName);
-            //}
-            //foreach (var product in productManager.GetAllByCategoryId(2))
-            //{
-            //    Console.WriteLine(product.ProductName);
-            //}
-            foreach (var product in productManager.GetByUnitPrice(50, 100))
-            {
-                Console.WriteLine(product.ProductName);
-            }
-        }
+        //private static void ProductTest()
+        //{
+        //    ProductManager productManager = new ProductManager(new EfProductDal());
+
+        //    //foreach (var product in productManager.GetAll())
+        //    //{
+        //    //    Console.WriteLine(product.ProductName);
+        //    //}
+        //    //foreach (var product in productManager.GetAllByCategoryId(2))
+        //    //{
+        //    //    Console.WriteLine(product.ProductName);
+        //    //}
+        //    foreach (var product in productManager.GetByUnitPrice(50, 100))
+        //    {
+        //        Console.WriteLine(product.ProductName);
+        //    }
+        //}
     }
 }
